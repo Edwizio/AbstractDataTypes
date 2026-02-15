2dd6f0395b5613a1f3bb429b7e53486465ac58d2
Zahra Rauf <zahra.aleem@gmail.com>
2026-02-15 19:49:31 +0100

    stack class defined

---
+++ stack.py
@@ -0,0 +1,22 @@
+class Stack:
+    """This class defines the Abstract Data Structure Stack and it's basic functionality"""
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return  self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return None
+
+    def size(self):
+        return len(self.items)
